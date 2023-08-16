7d7481a572389a20d3d3f157209447cc87878a24
marykulyk <masha.blyashinets@gmail.com>
2023-08-16 18:53:15 +0300

    innit commit

---
+++ lesson1-hw/main.js
@@ -0,0 +1,43 @@
+const s1  = 'hello';
+console.log(s1);
+const s2 = 'owu';
+console.log(s2);
+const s3 = 'com';
+console.log(s3);
+const s4 = 'ua';
+console.log(s4);
+const n1 = 1;
+console.log(n1);
+const n2 = 100;
+console.log(n2);
+const n3 = -999;
+console.log(n3);
+const n4 = 123;
+console.log(n4);
+const n5 = 3.14;
+console.log(n5);
+const n6 = 2.7;
+console.log(n6);
+const n7 = 16;
+console.log(n7);
+const b1 = true;
+console.log(b1)
+const b2 = false;
+console.log(b2)
+let firstName = 'Mariia',
+    middleName = 'Ivanivna',
+    lastName = 'Kulyk',
+    person = lastName + ' ' + firstName + ' ' + middleName;
+console.log(person);
+let a = 100;
+console.log(typeof a);
+let b = '100';
+console.log(typeof b);
+let c = true;
+console.log(typeof c);
+let 
+prompt("Введіть Ваше ім'я", firstName);
+console.log(firstName);
+prompt("Введіть Ваше по-батькові", middleName);
+console.log(middleName);
+
